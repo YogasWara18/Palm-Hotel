fbe6d54f8a46064d34408571e0c0364c51aad6b6
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-11-15 19:17:50 +0700

    Video hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -13,38 +13,37 @@ const Hero = () => {
         />
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div> */}
-       <div className="absolute inset-0">
+       <div className="relative h-screen w-full overflow-hidden text-white">
         <video
           autoPlay
           muted
           loop
           playsInline
-          className="object-cover object-center w-full h-full"
+          className=" absolute inset-0 object-cover w-full h-full"
         >
           <source src="/hero.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
         </video>
-        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute inset-0 bg-black/50"></div>
       </div>
 
 
-      <div className="relative flex flex-col justify-center items-center h-full text-center">
-        <h1 className="text-6xl font-extralight leading-tight mb-3 mt-35 capitalize">
-          Palm Hotel & Beach Club 
+      <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center px-4">
+        <h1 className="text-xl text-gray-300 font-light">
+          WELCOME TO
         </h1>
-        <p className="text-xl text-gray-300 mb-8 font-light">
-          Your Island of Comfort and Class
+        <p className="text-white text-4xl md:text-6xl mt-4 mb-8 font-extralight text-center">
+          PALM HOTEL & BEACH CLUB  
         </p>
-        <div className="flex gap-5">
+        <div className="flex flex-col md:flex-row gap-4">
           <Link
             href="#"
-            className="bg-yellow-500 text-white hover:bg-yellow-600 py-2.5 px-6 md:px-10 text-lg font-light hover:scale-105 hover:shadow-lg"
+            className="bg-yellow-500 text-white hover:bg-yellow-600 py-2.5 px-6 md:px-10 text-lg font-light hover:scale-105 hover:shadow-lg transition"
           >
             Book Now
           </Link>
           <Link
             href="/contact"
-            className="bg-transparent border border-yellow-500 text-white hover:bg-yellow-500 py-2.5 px-6 md:px-10 text-lg font-light hover:scale-105 hover:shadow-lg"
+            className="bg-transparent border border-yellow-500 text-white hover:bg-yellow-500 py-2.5 px-6 md:px-10 text-lg font-light hover:scale-105 hover:shadow-lg transition"
           >
             Contact Us
           </Link>
